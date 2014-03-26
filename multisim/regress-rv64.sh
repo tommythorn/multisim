11a8e5708349212dca66f2cd48c9b0caef368e32
Tommy Thorn <tommy-multisim-github@thorn.ws>
2014-03-25 23:43:01 -0700

    begin work on the priviledged support

--- multisim/regress-rv64.sh
+++ multisim/regress-rv64.sh
@@ -2,7 +2,8 @@
 
 RISCV_TOOLS=$HOME/BTSync/riscv-tools
 RV32_ISA=$RISCV_TOOLS/riscv-tests/isa
-for x in $RV32_ISA/rv64ui-p-*.hex
+for x in $RV32_ISA/rv64ui-{p,v}-*.hex
 do
+    echo $x 1>&2
     echo $(./multisim $(echo $x|sed 's,.hex$,,') 2>&1 |grep 'HOST RESULT') $x
-done | sort | grep -v 'HOST RESULT 1 ' | tee regressions64.log
+done | tee raw.log | sort | grep -v 'HOST RESULT 1 ' | tee regressions64.log
